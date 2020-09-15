e6f43ef6b7c9d8324fbff38bf84e7bf75d2681b9
Kibaek Kim <kibaek.k@gmail.com>
2020-09-15 13:57:20 -0500

    cleanup test

---
+++ test/dcap.jl
@@ -0,0 +1,106 @@
+#=
+Source:
+  S. Ahmed and R. Garcia. "Dynamic Capacity Acquisition and Assignment under Uncertainty," Annals of Operations Research, vol.124, pp. 267-283, 2003
+
+Input:
+  nR: number of resources
+  nN: number of tasks
+  nT: number of time periods
+  nS: number of scenarios
+
+Sets:
+  R: resources
+  N: tasks
+  T: time periods
+  S: scenarios
+
+Variables (1st Stage):
+  x[i,t]: capacity acquired for resource i at period t
+  u[i,t]: 1 if x[i,t] > 0, 0 otherwise
+
+Variables (2nd Stage):
+  y[i,j,t]: 1 if resource i is assigned to task j in period t, 0 otherwise
+
+Parameters (general):
+  a[i,t]: linear component of expansion cost for resource i at period t
+  b[i,t]: fixed component of expansion cost for resource i at period t
+  c[i,j,t,s]: cost of assigning resource i to task j in period t
+  c0[j,t,s]: penalty incurred if task j in period t is not served
+
+Parameters (scenario):
+  d[j,t,s]: capacity required for to perform task j in period t in scenario s
+=#
+
+using StructJuMP
+using Random
+
+function DCAP(nR::Int, nN::Int, nT::Int, nS::Int, seed::Int=1)::StructuredModel
+
+    # set random seed (default=1)
+    Random.seed!(seed)
+
+    # generate & store instance data
+    ## sets
+    R = 1:nR
+    N = 1:nN
+    T = 1:nT
+    S = 1:nS
+
+    ## parameters
+    a = rand(nR, nT) * 5 .+ 5
+    b = rand(nR, nT) * 40 .+ 10
+    c = rand(nR, nN, nT, nS) * 5 .+ 5
+    c0 = rand(nN, nT, nS) * 500 .+ 500
+    d = rand(nN, nT, nS) .+ 0.5
+    Pr = ones(nS)/nS
+
+    # construct JuMP.Model
+    model = StructuredModel(num_scenarios = nS)
+
+    ## 1st stage
+    @variable(model, x[i=R,t=T] >= 0)
+    @variable(model, u[i=R,t=T], Bin)
+    @objective(model, Min, sum(a[i,t]*x[i,t] + b[i,t]*u[i,t] for i in R for t in T))
+    @constraint(model, [i=R,t=T], x[i,t] - u[i,t] <= 0)
+
+    ## 2nd stage
+    for s in S
+        sb = StructuredModel(parent=model, id = s, prob = Pr[s])
+        @variable(sb, y[i=R, j=N, t=T], Bin)
+        #@variable(sb, z[j=N,t=T] >= 0) # originally implemented variable (continuous)
+        @variable(sb, z[j=N,t=T], Bin)  # modify as SIPLIB 1.0
+        @objective(sb, Min, sum(c[i,j,t,s]*y[i,j,t] for i in R for j in N for t in T) + sum(c0[j,t,s]*z[j,t] for j in N for t in T))
+        @constraint(sb, [i=R, t=T], -sum(x[i,tau] for tau in 1:t) + sum(d[j,t,s]*y[i,j,t] for j in N) <= 0)
+        @constraint(sb, [j=N, t=T], sum(y[i,j,t] for i in R) + z[j,t] == 1)
+    end
+
+    return model
+end
+
+m = DCAP(2,2,3,3);
+
+@testset "Parent model" begin
+  @test length(m.variables) == 12
+  @test length(m.constraints) == 6
+  start, index, value, rlbd, rubd, obj, clbd, cubd, ctype, cname = DSPopt.get_model_data(m)
+  @show clbd
+  @show cubd
+  @show ctype
+  # print(m)
+end
+
+@testset "Child model $i" for (i,subm) in m.children
+  start, index, value, rlbd, rubd, obj, clbd, cubd, ctype, cname = DSPopt.get_model_data(subm)
+  @show clbd
+  @show cubd
+  @show ctype
+end
+
+@testset "write MPS" begin
+  DSPopt.writeMps!(m, "dcap")
+  @test isfile("dcap.mps")
+end
+
+@testset "optimize!: $j" for j in [DSPopt.ExtensiveForm]
+  status = DSPopt.optimize!(m, solve_type = j, is_stochastic = true, param = "params.txt")
+end
--- test/farmer_block.jl
+++ test/farmer_block.jl
@@ -1,3 +1,4 @@
+using DSPopt
 using StructJuMP
 
 NS = 3;                        # number of scenarios
@@ -30,8 +31,114 @@ m = StructuredModel(num_scenarios = NS)
 for s in 1:NS
     blk = StructuredModel(parent = m, id = s)
 	@objective(blk, Min, 0.)
-	@constraint(blk, const_budget, sum(x[i,s] for i=CROPS) <= Budget)
+	@constraint(blk, sum(x[i,s] for i=CROPS) <= Budget)
     @constraint(blk, const_minreq[j=PURCH], Yield[s,j] * x[j,s] + y[j,s] - w[j,s] >= Minreq[j])
     @constraint(blk, const_minreq_beets, Yield[s,3] * x[3,s] - w[3,s] - w[4,s] >= Minreq[3])
     @constraint(blk, const_aux, w[3,s] <= 6000)
+end
+
+"""
+NOTE: 
+  JuMP v0.21 changes the order of indexing variables.
+  This affects some of the test lines below, which were based on the older variable indexing.
+"""
+
+@testset "Parent model" begin
+    @test length(m.variables) == 27
+    @test length(m.constraints) == 6
+    start, index, value, rlbd, rubd, obj, clbd, cubd, ctype, cname = DSPopt.get_model_data(m)
+    @test length(start) == length(m.constraints) + 1
+    @test start[end] == length(index)
+    @test start == [0, 2, 4, 6, 8, 10, 12]
+    # @test index == [0, 1, 1, 2, 3, 4, 4, 5, 6, 7, 7, 8] # will fail with JuMP v0.21
+    @test value == [1., -1., 1., -1., 1., -1., 1., -1., 1., -1., 1., -1.]
+    @test rlbd == zeros(6)
+    @test rubd == zeros(6)
+    # @test obj == [
+    #     probability[1]*Cost[1], probability[2]*Cost[1], probability[3]*Cost[1],
+    #     probability[1]*Cost[2], probability[2]*Cost[2], probability[3]*Cost[2],
+    #     probability[1]*Cost[3], probability[2]*Cost[3], probability[3]*Cost[3],
+    #     probability[1]*Purchase[1], probability[2]*Purchase[1], probability[3]*Purchase[1],
+    #     probability[1]*Purchase[2], probability[2]*Purchase[2], probability[3]*Purchase[2],
+    #     -probability[1]*Sell[1], -probability[2]*Sell[1], -probability[3]*Sell[1],
+    #     -probability[1]*Sell[2], -probability[2]*Sell[2], -probability[3]*Sell[2],
+    #     -probability[1]*Sell[3], -probability[2]*Sell[3], -probability[3]*Sell[3],
+    #     -probability[1]*Sell[4], -probability[2]*Sell[4], -probability[3]*Sell[4]] # will fail with JuMP v0.21
+    @test clbd == zeros(27)
+    @test cubd == zeros(27) .+ Inf
+    @test ctype == "IIIIIIIIICCCCCCCCCCCCCCCCCC"
+end
+
+@testset "Child model $i" for (i,subm) in m.children
+    @test i > 0
+    @test length(subm.variables) == 0
+    @test length(subm.constraints) == 5
+    start, index, value, rlbd, rubd, obj, clbd, cubd, ctype, cname = DSPopt.get_model_data(subm)
+    @test length(start) == length(subm.constraints) + 1
+    @test start[end] == length(index)
+    @test start == [0, 3, 6, 9, 12, 13]
+    # @test index == [0, 3, 6,  0, 9, 15,  3, 12, 18,  6, 21, 24,  21] .+ (i-1) # will fail with JuMP v0.21
+    @test value == [1., 1., 1.,  Yield[i,1], 1., -1.,  Yield[i,2], 1., -1.,  Yield[i,3], -1., -1., 1.]
+    @test rlbd == [-Inf; Minreq; -Inf]
+    @test rubd == [Budget, Inf, Inf, Inf, 6000]
+    @test obj == []
+    @test clbd == []
+    @test cubd == []
+    @test ctype == ""
+end
+
+@testset "optimize!: $j" for j in [DSPopt.ExtensiveForm, DSPopt.Dual]
+    status = DSPopt.optimize!(m, solve_type = j, is_stochastic = false)
+    @test status == MOI.OPTIMAL
+    @test isapprox(objective_value(m), -108390.)
+    @test isapprox(dual_objective_value(m), -108390.)
+
+    primsol = value()
+    dualsol = dual()
+    for s = 1:3
+        @test primsol[s] == []
+    end
+    @test dualsol == []
+    @test isapprox(value(x[1,1]), 170.0)
+    @test isapprox(value(x[1,2]), 170.0)
+    @test isapprox(value(x[1,3]), 170.0)
+    @test isapprox(value(x[2,1]), 80.0)
+    @test isapprox(value(x[2,2]), 80.0)
+    @test isapprox(value(x[2,3]), 80.0)
+    @test isapprox(value(x[3,1]), 250.0)
+    @test isapprox(value(x[3,2]), 250.0)
+    @test isapprox(value(x[3,3]), 250.0)
+    @test isapprox(value(y[1,1]), 0.0)
+    @test isapprox(value(y[1,2]), 0.0)
+    @test isapprox(value(y[1,3]), 0.0)
+    @test isapprox(value(y[2,1]), 0.0)
+    @test isapprox(value(y[2,2]), 0.0)
+    @test isapprox(value(y[2,3]), 48.0)
+    @test isapprox(value(w[1,1]), 310.0)
+    @test isapprox(value(w[1,2]), 225.0)
+    @test isapprox(value(w[1,3]), 140.0)
+    @test isapprox(value(w[2,1]), 48.0)
+    @test isapprox(value(w[2,2]), 0.0)
+    @test isapprox(value(w[2,3]), 0.0)
+    @test isapprox(value(w[3,1]), 6000.0)
+    @test isapprox(value(w[3,2]), 5000.0)
+    @test isapprox(value(w[3,3]), 4000.0)
+    @test isapprox(value(w[4,1]), 0.0)
+    @test isapprox(value(w[4,2]), 0.0)
+    @test isapprox(value(w[4,3]), 0.0)
+    DSPopt.freeSolver(dsp)
+    @testset "freeModel" begin
+        DSPopt.freeModel(dsp)
+        @test dsp.p != C_NULL
+        @test length(dsp.numRows) == 0
+        @test length(dsp.numCols) == 0
+        @test isnan(dsp.primVal)
+        @test isnan(dsp.dualVal)
+        @test length(dsp.colVal) == 0
+        @test length(dsp.rowVal) == 0
+        @test dsp.nblocks == -1
+        @test dsp.block_ids == []
+        @test dsp.is_stochastic == false
+        @test dsp.solve_type == DSPopt.Dual
+    end
 end
