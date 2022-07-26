a7731f352a9acc72e2767f832bb4a7727a8e40db
Kibaek Kim <kibaek.k@gmail.com>
2022-07-25 22:23:02 -0400

    moved and fixed

--- examples/multistage.jl
+++ examples/multistage.jl
@@ -1,16 +1,7 @@
-using Pkg
-Pkg.activate(".");
-
-Pkg.add("DSP")
-Pkg.add("DSPopt")
-Pkg.add("StructJuMP")
-Pkg.add("MPI")
-Pkg.add("ScenTrees")
-
 using MPI
+using ScenTrees # This package should be added before the package below. I don't know why...
 using StructJuMP
 using DSPopt
-using ScenTrees
 
 # scenario tree w/ 3 stages, each parent node has 2 children; dim = 1
 tree = Tree([1,2,2],1);
@@ -35,17 +26,23 @@ model = StructuredModel(num_scenarios = 4)
 @constraint(model, x[1,0] + w[1,0] - y[1,0] == d[1,1]) # demand at root node
 @constraint(model, x[1,0] <= 2) # production capacity at root node
 
-@objective(model, Min, x[1,0] + 3*w[1,0] + 0.5*y[1,0])
+@objective(model, Min, 
+    x[1,0] + 3*w[1,0] + 0.5*y[1,0]
+    + 0.5 * sum(x[k,1] + 3*w[k,1] + 0.5*y[k,1] for k in 2:3) 
+    + 0.25 * sum(x[k,2] + 3*w[k,2] for k in 4:7)
+)
 
 for m = 2:length(nodes(tree))
     # create a StructuredModel linked to model with id m
-    blk = StructuredModel(parent = model, id = m)
+    blk = StructuredModel(parent = model, id = m-1)
 
     # OBJECTIVE
-    @objective(blk, Min, 0.5*sum(x[k,1] + 3*w[k,1] + 0.5*y[k,1] for k in 2:3) + 0.25*sum(x[k,2] + 3*w[k,2] for k in 4:7))
+    @objective(blk, Min, 0.)
 
     # CONSTRAINTS
     for t in stage(tree)[m]
+        println("x[$m,$t]")
+        println("y[$(root(tree,m)[t]),$(t-1)] + x[$m,$t] + w[$m,$t] - y[$m,$t] == d[$m,$(t+1)]")
         @constraint(blk, x[m,t] <= 2) # production capacity
         @constraint(blk, y[root(tree,m)[t],t-1] + x[m,t] + w[m,t] - y[m,t] == d[m,t+1]) # demand
     end
