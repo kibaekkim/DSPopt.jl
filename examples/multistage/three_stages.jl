1a7cfc8d7120c1edc38a0979296990e72a9675fc
rachaelalfant <55371799+rachaelalfant@users.noreply.github.com>
2022-08-02 15:37:19 -0500

    Update three_stages.jl

--- examples/multistage/three_stages.jl
+++ examples/multistage/three_stages.jl
@@ -19,27 +19,27 @@ function deterministic_form()
     @variable(model, w[n in nodes(tree),t in stage(tree)[n]] >= 0, Int)
     @variable(model, y[n in nodes(tree),t in stage(tree)[n]] >= 0, Int)
 
-    # OBJECTIVE
+    # OBJECTIVE (parent)
     @objective(model, Min,
         x[1,0] + 3*w[1,0] + 0.5*y[1,0]
         + sum(0.5*(x[k,1] + 3*w[k,1] + 0.5*y[k,1]) for k in 2:3)
         + sum(0.25*(x[k,2] + 3*w[k,2]) for k in 4:7)
     )
 
-    # CONSTRAINTS
-    @constraint(model, x[1,0] + w[1,0] - y[1,0] == d[1]) # demand at root node
-    @constraint(model, x[1,0] <= 2) # production capacity at root node
+    for m = 1:length(nodes(tree))
+        # create a StructuredModel linked to model with id m
+        blk = StructuredModel(parent = model, id = m)
 
-    for m = 2:length(nodes(tree))
-        # create a StructuredModel linked to model with id m-1
-        blk = StructuredModel(parent = model, id = m-1)
-
-        # OBJECTIVE
+        # OBJECTIVE (subprob)
         @objective(blk, Min, 0.)
 
-        # CONSTRAINTS
+        # CONSTRAINTS (subprob)
         @constraint(blk, [t=stage(tree)[m]], x[m,t] <= 2) # production capacity
-        @constraint(blk, [t=stage(tree)[m]], y[root(tree,m)[t],t-1] + x[m,t] + w[m,t] - y[m,t] == d[m]) # demand
+        if m == 1 # demand
+            @constraint(model, x[m,0] + w[m,0] - y[m,0] == d[m])
+        else
+            @constraint(blk, [t=stage(tree)[m]], y[root(tree,m)[t],t-1] + x[m,t] + w[m,t] - y[m,t] == d[m])
+        end
     end
 
     status = optimize!(model,
@@ -67,7 +67,7 @@ function deterministic_form_with_nonant()
     @variable(model, w[n in nodes(tree),t=0:T-1] >= 0, Int)
     @variable(model, y[n in nodes(tree),t=0:T-1] >= 0, Int)
 
-    # OBJECTIVE
+    # OBJECTIVE (parent)
     @objective(model, Min,
         (1/3) * sum(
             (x[1,t] + 3*w[1,t] + 0.5*y[1,t]) +
@@ -76,26 +76,26 @@ function deterministic_form_with_nonant()
         )
     )
 
-    # CONSTRAINTS
-    @constraint(model, x[1,0] + w[1,0] - y[1,0] == d[1]) # demand at root node
-    @constraint(model, x[1,0] <= 2) # production capacity at root node
+    # CONSTRAINTS (parent)
     # non-anticipativity
     @constraint(model, [n=nodes(tree),t=1:T-1], x[n,t-1] == x[n,t])
     @constraint(model, [n=nodes(tree),t=1:T-1], w[n,t-1] == w[n,t])
     @constraint(model, [n=nodes(tree),t=1:T-1], y[n,t-1] == y[n,t])
 
-    for m = 2:length(nodes(tree))
-        # create a StructuredModel linked to model with id m-1
-        blk = StructuredModel(parent = model, id = m-1)
+    for m = 1:length(nodes(tree))
+        # create a StructuredModel linked to model with id m
+        blk = StructuredModel(parent = model, id = m)
 
-        # OBJECTIVE
+        # OBJECTIVE (subprob)
         @objective(blk, Min, 0.)
 
-        # CONSTRAINTS
+        # CONSTRAINTS (subprob)
         @constraint(blk, [t=0:T-1], x[m,t] <= 2) # production capacity
-        if m in 2:3 # demand
+        if m == 1 # demand
+            @constraint(model, x[m,0] + w[m,0] - y[m,0] == d[m])
+        elseif m in 2:3
             @constraint(blk, y[root(tree,m)[1],0] + x[m,1] + w[m,1] - y[m,1] == d[m])
-        elseif m in 4:7
+        else
             @constraint(blk, y[root(tree,m)[2],1] + x[m,2] + w[m,2] - y[m,2] == d[m])
         end
     end
