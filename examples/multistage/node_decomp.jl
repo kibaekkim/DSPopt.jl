0fbdbf6b3ca553d90d3716c5cae44778694176bc
rachaelalfant <55371799+rachaelalfant@users.noreply.github.com>
2022-08-03 17:36:59 -0500

    generalized nonanticipativity and objective

--- examples/multistage/node_decomp.jl
+++ examples/multistage/node_decomp.jl
@@ -20,23 +20,20 @@ function deterministic_form()
 
     # OBJECTIVE
     @objective(model, Min,
-        x[1,0] + 3*w[1,0] + 0.5*y[1,0]
-        + sum(0.5*(x[k,1] + 3*w[k,1] + 0.5*y[k,1]) for k in 2:3)
-        + sum(0.25*(x[k,2] + 3*w[k,2]) for k in 4:7)
-    )
+        x[1,0] + 3*w[1,0] + 0.5*y[1,0] +
+        (1/2)*sum(
+            x[n,stage(tree)[n]] + 3*w[n,stage(tree)[n]] + 0.5*y[n,stage(tree)[n]]
+            for n in 2:3) +
+        (1/4)*sum(x[n,stage(tree)[n]] + 3*w[n,stage(tree)[n]] for n in 4:7)
+            )
 
     # CONSTRAINTS
-    for n = 1:length(nodes(tree))
-        # production capacity
-        @constraint(model, [t in stage(tree)[n]], x[n,t] <= 2)
-        # demand
-        if n == 1
-            @constraint(model, x[n,0] + w[n,0] - y[n,0] == d[n])
-        else
-            @constraint(model, [t in stage(tree)[n]],
-                y[root(tree,n)[t],t-1] + x[n,t] + w[n,t] - y[n,t] == d[n])
-        end
-    end
+    # production capacity
+    @constraint(model, [n=1:length(nodes(tree)), t=stage(tree)[n]], x[n,t] <= 2)
+    # demand
+    @constraint(model, x[1,0] + w[1,0] - y[1,0] == d[1])
+    @constraint(model, [n=2:length(nodes(tree)), t=stage(tree)[n]],
+        y[root(tree,n)[t],t-1] + x[n,t] + w[n,t] - y[n,t] == d[n])
 
     optimize!(model)
     @show termination_status(model)
@@ -58,23 +55,23 @@ function deterministic_form_with_nonant()
 
     # OBJECTIVE
     @objective(model, Min,
-        sum(
-            (x[1,0] + 3*w[1,0] + 0.5*y[1,0]) +
-            (1/2)*sum(x[n,1] + 3*w[n,1] + 0.5*y[n,1] for n=2:3) +
-            (1/4)*sum(x[n,2] + 3*w[n,2] for n=4:7)
+        (x[1,0] + 3*w[1,0] + 0.5*y[1,0]) +
+        (1/2)*sum(
+            x[n,stage(tree)[n]] + 3*w[n,stage(tree)[n]] + 0.5*y[n,stage(tree)[n]]
+                for n=2:3) +
+        (1/4)*sum(x[n,stage(tree)[n]] + 3*w[n,stage(tree)[n]] for n=4:7)
             )
-    )
 
     # CONSTRAINTS
     # non-anticipativity
-    @constraint(model, [n=1:2], y[n,0] == y[n+1,0])
-    @constraint(model, [n=4:5], y[2,1] == y[n,1])
-    @constraint(model, [n=6:7], y[3,1] == y[n,1])
+    @constraint(model, [n=2:length(nodes(tree))],
+        y[Int(floor(n/2)), stage(tree)[n]-1] == y[n, stage(tree)[n]-1])
     # production capacity
     @constraint(model, [n=1:length(nodes(tree)), t=0:T-1], x[n,t] <= 2)
     # demand
     @constraint(model, x[1,0] + w[1,0] - y[1,0] == d[1])
-    @constraint(model, [n=2:length(nodes(tree)), t=stage(tree)[n]], y[n,t-1] + x[n,t] + w[n,t] - y[n,t] == d[n])
+    @constraint(model, [n=2:length(nodes(tree)), t=stage(tree)[n]],
+        y[n,t-1] + x[n,t] + w[n,t] - y[n,t] == d[n])
 
     optimize!(model)
     @show termination_status(model)
