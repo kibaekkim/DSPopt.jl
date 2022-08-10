28ef06959f80e8761068ff39f6059cc90fbea52d
rachaelalfant <55371799+rachaelalfant@users.noreply.github.com>
2022-08-10 15:33:01 -0500

    removed scentrees

--- examples/multistage/three_stages.jl
+++ examples/multistage/three_stages.jl
@@ -1,42 +1,57 @@
-using ScenTrees # This package should be added before the package below. I don't know why...
 using StructJuMP
 using DSPopt
 using MPI
 
-# scenario tree w/ 3 stages, each parent node has 2 children; dim = 1
-tree = Tree([1,2,2],1);
-T = 3; # number of stages
+T = 3; # number of stages - MUST BE >= 3 !!
 
-d = [1,1,3,1,3,1,3] # demand vector
+# scenario tree w/ T stages
+nodes = 1:(2^T)-1; # nodes in tree
+stage = Vector{Int64}(undef, (2^T)-1); # stage of each node in tree
+for i in nodes, t in 1:T
+    if i == 1 # stage for root node
+        stage[i] = 0;
+    elseif i in (2^t):2^(t+1)-1 # stage for all other nodes
+        stage[i] = t;
+    end
+end
+
+d = Vector{Int64}(undef, (2^T)-1); # demand vector
+for i in nodes
+    if i == 1
+        d[i] = 1; # demand at root node
+    elseif (i % 2 == 0)
+        d[i] = 1; # demand at even-numbered nodes
+    else
+        d[i] = 3; # demand at odd-numbered nodes
+    end
+end
 
 MPI.Init()
 DSPopt.parallelize(MPI.COMM_WORLD)
 
 # create StructuredModel with number of scenarios
-model = StructuredModel(num_scenarios = length(leaves(tree)[2]))
+model = StructuredModel(num_scenarios = 2^(T-1))
 
 ystages = [[0], [0,1], [1,2]];
 
 # VARIABLES
-@variable(model, x[n in nodes(tree),t in stage(tree)[n]] >= 0, Int)
-@variable(model, w[n in nodes(tree),t in stage(tree)[n]] >= 0, Int)
-@variable(model, y[n in 1:length(nodes(tree)),t=ystages[stage(tree)[n]+1]] >= 0, Int)
+@variable(model, x[n in nodes, t in stage[n]] >= 0, Int)
+@variable(model, w[n in nodes, t in stage[n]] >= 0, Int)
+@variable(model, y[n in 1:(2^T)-1,t=ystages[stage[n]+1]] >= 0, Int)
 
 # OBJECTIVE (parent)
 @objective(model, Min,
     (x[1,0] + 3*w[1,0] + 0.5*y[1,0]) +
-    (1/2)*sum(
-        x[n,stage(tree)[n]] + 3*w[n,stage(tree)[n]] + 0.5*y[n,stage(tree)[n]]
-            for n=2:3) +
-    (1/4)*sum(x[n,stage(tree)[n]] + 3*w[n,stage(tree)[n]] for n=4:7)
+    (1/2)*sum(x[n,stage[n]] + 3*w[n,stage[n]] + 0.5*y[n,stage[n]] for n=2:3) +
+    (1/4)*sum(x[n,stage[n]] + 3*w[n,stage[n]] + 0.5*y[n,stage[n]] for n=4:7)
         )
 
 # CONSTRAINTS (parent)
 # non-anticipativity
-@constraint(model, [n=2:length(nodes(tree))],
-    y[Int(floor(n/2)), stage(tree)[n]-1] == y[n, stage(tree)[n]-1])
+@constraint(model, [n=2:(2^T)-1],
+    y[Int(floor(n/2)), stage[n]-1] == y[n, stage[n]-1])
 
-for m = 1:length(nodes(tree))
+for m = 1:(2^T)-1
     # create a StructuredModel linked to model with id m
     blk = StructuredModel(parent = model, id = m)
 
@@ -45,16 +60,15 @@ for m = 1:length(nodes(tree))
 
     # CONSTRAINTS (subprob)
     # production capacity
-    @constraint(blk, [t=stage(tree)[m]], x[m,t] <= 2)
-    @constraint(blk, [t=stage(tree)[m]], w[m,t] <= maximum(d)) # need upper bounds on all vars for some reason
-    @constraint(blk, [t=stage(tree)[m]], y[m,t] <= maximum(d))
+    @constraint(blk, [t=stage[m]], x[m,t] <= 2)
+    @constraint(blk, [t=stage[m]], w[m,t] <= maximum(d)) # need upper bounds on all vars for some reason
+    @constraint(blk, [t=stage[m]], y[m,t] <= maximum(d))
 
     # demand
     if m == 1
         @constraint(blk, x[m,0] + w[m,0] - y[m,0] == d[m])
     else
-        @constraint(blk, [t=stage(tree)[m]],
-            y[m,t-1] + x[m,t] + w[m,t] - y[m,t] == d[m])
+        @constraint(blk, [t=stage[m]], y[m,t-1] + x[m,t] + w[m,t] - y[m,t] == d[m])
     end
 end
 
