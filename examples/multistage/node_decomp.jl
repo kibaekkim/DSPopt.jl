9cee4e03ca7267701c0f2120023d7f7fc7f9f133
rachaelalfant <55371799+rachaelalfant@users.noreply.github.com>
2022-08-10 15:42:52 -0500

    only use scentrees for deterministic_form()

--- examples/multistage/node_decomp.jl
+++ examples/multistage/node_decomp.jl
@@ -2,14 +2,13 @@ using ScenTrees
 using JuMP
 using GLPK
 
-# scenario tree w/ 3 stages, each parent node has 2 children; dim = 1
-tree = Tree([1,2,2],1);
-T = 3; # number of stages
-
 d = [1, 1, 3, 1, 3, 1, 3]; # demand
 
-
 function deterministic_form()
+
+    # scenario tree w/ 3 stages, each parent node has 2 children; dim = 1
+    tree = Tree([1,2,2],1);
+
     model = Model(GLPK.Optimizer)
 
     # VARIABLES
@@ -46,33 +45,47 @@ end
 
 
 function deterministic_form_with_nonant()
+
+    T = 3; # number of stages - MUST BE >= 3 !!
+
+    # scenario tree w/ T stages
+    nodes = 1:(2^T)-1; # nodes in tree
+    stage = Vector{Int64}(undef, (2^T)-1); # stage of each node in tree
+    for i in nodes, t in 1:T
+        if i == 1 # stage for root node
+            stage[i] = 0;
+        elseif i in (2^t):2^(t+1)-1 # stage for all other nodes
+            stage[i] = t;
+        end
+    end
+
     model = Model(GLPK.Optimizer)
 
     ystages = [[0], [0,1], [1,2]];
 
     # VARIABLES
-    @variable(model, x[n in nodes(tree),t in stage(tree)[n]] >= 0, Int)
-    @variable(model, w[n in nodes(tree),t in stage(tree)[n]] >= 0, Int)
-    @variable(model, y[n in 1:length(nodes(tree)),t=ystages[stage(tree)[n]+1]] >= 0, Int)
+    @variable(model, x[n in nodes, t in stage[n]] >= 0, Int)
+    @variable(model, w[n in nodes, t in stage[n]] >= 0, Int)
+    @variable(model, y[n in 1:(2^T)-1, t=ystages[stage[n]+1]] >= 0, Int)
 
     # OBJECTIVE
     @objective(model, Min,
         (x[1,0] + 3*w[1,0] + 0.5*y[1,0]) +
         (1/2)*sum(
-            x[n,stage(tree)[n]] + 3*w[n,stage(tree)[n]] + 0.5*y[n,stage(tree)[n]]
+            x[n,stage[n]] + 3*w[n,stage[n]] + 0.5*y[n,stage[n]]
                 for n=2:3) +
-        (1/4)*sum(x[n,stage(tree)[n]] + 3*w[n,stage(tree)[n]] for n=4:7)
+        (1/4)*sum(x[n,stage[n]] + 3*w[n,stage[n]] for n=4:7)
             )
 
     # CONSTRAINTS
     # non-anticipativity
-    @constraint(model, [n=2:length(nodes(tree))],
-        y[Int(floor(n/2)), stage(tree)[n]-1] == y[n, stage(tree)[n]-1])
+    @constraint(model, [n=2:(2^T)-1],
+        y[Int(floor(n/2)), stage[n]-1] == y[n, stage[n]-1])
     # production capacity
-    @constraint(model, [n=1:length(nodes(tree)), t=stage(tree)[n]], x[n,t] <= 2)
+    @constraint(model, [n=1:(2^T)-1, t=stage[n]], x[n,t] <= 2)
     # demand
     @constraint(model, x[1,0] + w[1,0] - y[1,0] == d[1])
-    @constraint(model, [n=2:length(nodes(tree)), t=stage(tree)[n]],
+    @constraint(model, [n=2:(2^T)-1, t=stage[n]],
         y[n,t-1] + x[n,t] + w[n,t] - y[n,t] == d[n])
 
     optimize!(model)
